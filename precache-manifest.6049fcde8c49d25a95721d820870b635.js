60b87ed0c9d2579d1f5b58526f620065b969a5ad
Miguel Angel <miguelapb35@gmail.com>
2020-04-06 15:07:18 -0500

    Updates

---
+++ precache-manifest.6049fcde8c49d25a95721d820870b635.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "292116349b4ab2a309393b895dc1ae7b",
+    "url": "/paginationpokelist/index.html"
+  },
+  {
+    "revision": "30f6c33df81198256cb2",
+    "url": "/paginationpokelist/static/js/2.a78e376f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/paginationpokelist/static/js/2.a78e376f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f50b3bf3ad47fb8cbdd",
+    "url": "/paginationpokelist/static/js/main.e7cf742d.chunk.js"
+  },
+  {
+    "revision": "86a8fb2c2bdf6ef488e9",
+    "url": "/paginationpokelist/static/js/runtime-main.f43aa43c.js"
+  }
+]);
